4809ef91f2ac7f0d17a15b338ef8e2cd6e5a885b
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-18 15:59:03 +0530

    feat: add CustomerService interface, implement CustomerService methods

---
+++ src/main/java/edu/icet/service/CustomerService.java
@@ -0,0 +1,12 @@
+package edu.icet.service;
+
+import edu.icet.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    boolean addCustomer(Customer customer);
+    boolean updateCustomer(Customer customer);
+    boolean deleteCustomer(Customer customer);
+    List<Customer>getAll();
+}
