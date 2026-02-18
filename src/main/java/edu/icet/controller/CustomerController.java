10324873cca2a1fc164f65a3a65f2c5d731d7417
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-18 16:06:46 +0530

    feat: service constructor inject

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,12 +1,18 @@
 package edu.icet.controller;
 
+import edu.icet.model.Customer;
+import edu.icet.service.CustomerService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/customer")
 @Tag(name = "Customer-Controller")
 public class CustomerController {
+
+    final private CustomerService service;
 
 }
