c0957551469c9ed72b775840acd6fd56ddc472d3
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-19 01:54:34 +0530

    feat: create controller layer

---
+++ src/main/java/edu/icet/controller/ItemController.java
@@ -0,0 +1,11 @@
+package edu.icet.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/item")
+@Tag(name = "Item-Controller")
+public class ItemController {
+}
