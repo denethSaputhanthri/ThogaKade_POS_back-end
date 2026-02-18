c4f8d10c97258645832afde63c1a6dbf1eea6376
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-18 16:55:31 +0530

    feat: implement customer Service  crud

--- src/main/java/edu/icet/service/impl/CustomerServiceImpl.java
+++ src/main/java/edu/icet/service/impl/CustomerServiceImpl.java
@@ -1,31 +1,36 @@
 package edu.icet.service.impl;
 
 import edu.icet.model.Customer;
+import edu.icet.repository.CustomerRepository;
 import edu.icet.service.CustomerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
+
+    final private CustomerRepository repository;
 
     @Override
     public boolean addCustomer(Customer customer) {
-        return false;
+        return repository.addCustomer(customer);
     }
 
     @Override
     public boolean updateCustomer(Customer customer) {
-        return false;
+        return repository.updateCustomer(customer);
     }
 
     @Override
-    public boolean deleteCustomer(Customer customer) {
-        return false;
+    public boolean deleteCustomer(Integer id) {
+        return repository.deleteCustomer(id);
     }
 
     @Override
     public List<Customer> getAll() {
-        return List.of();
+        return repository.getAll();
     }
 }
