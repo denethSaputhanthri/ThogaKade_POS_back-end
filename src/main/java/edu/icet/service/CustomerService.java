524fd8215ace9f36a3d6987479256ac9c6de2dbd
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-18 23:27:27 +0530

    feat: change parameters;

--- src/main/java/edu/icet/service/CustomerService.java
+++ src/main/java/edu/icet/service/CustomerService.java
@@ -7,6 +7,6 @@ import java.util.List;
 public interface CustomerService {
     boolean addCustomer(Customer customer);
     boolean updateCustomer(Customer customer);
-    boolean deleteCustomer(Customer customer);
+    boolean deleteCustomer(Integer id);
     List<Customer>getAll();
 }
