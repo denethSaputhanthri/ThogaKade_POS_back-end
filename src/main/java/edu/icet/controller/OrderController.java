6a7c302dc18e421527ef148eaf92fc129eeb0ff6
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-19 08:56:15 +0530

    feat: create order controller class

---
+++ src/main/java/edu/icet/controller/OrderController.java
@@ -0,0 +1,13 @@
+package edu.icet.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/order")
+@Tag(name = "Order-Controller")
+public class OrderController {
+}
