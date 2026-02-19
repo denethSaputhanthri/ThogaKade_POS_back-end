f50d2c9bb7c573f4ac5f8787c80f618a6cf04231
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-19 08:45:59 +0530

    feat: variable name change

--- src/main/java/edu/icet/model/Order.java
+++ src/main/java/edu/icet/model/Order.java
@@ -3,7 +3,7 @@ package edu.icet.model;
 import java.time.LocalDate;
 
 public class Order {
-    private String orderId;
+    private String id;
     private LocalDate orderDate;
     private String customerId;
 }
